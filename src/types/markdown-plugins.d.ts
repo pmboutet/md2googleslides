4189fa5e6084c1c95a14d72d0c12b285e56049c3
pmboutet <37446103+pmboutet@users.noreply.github.com>
2025-07-12 22:41:26 +0200

    Add TypeScript declaration file for CommonJS modules

---
+++ src/types/markdown-plugins.d.ts
@@ -0,0 +1,7 @@
+# Declaration files for CommonJS modules without proper TypeScript declarations
+declare module 'markdown-it-attrs';
+declare module 'markdown-it-lazy-headers'; 
+declare module 'markdown-it-emoji';
+declare module 'markdown-it-expand-tabs';
+declare module 'markdown-it-video';
+declare module 'markdown-it-container';
